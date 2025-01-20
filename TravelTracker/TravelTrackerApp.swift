08118dabb651fd42440155dca337dc7b48d16ce2
mmohnsen <memohnsen@gmail.com>
2025-01-20 12:15:44 -0500

    Initial Commit

---
+++ TravelTracker/TravelTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTrackerApp.swift
+//  TravelTracker
+//
+//  Created by Maddisen Mohnsen on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelTrackerTests/TravelTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTrackerTests.swift
+//  TravelTrackerTests
+//
+//  Created by Maddisen Mohnsen on 1/20/25.
+//
+
+import Testing
+@testable import TravelTracker
+
+struct TravelTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
